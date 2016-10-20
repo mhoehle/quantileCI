d3eff0941902e3e7b8f3bda277ce206ac08777bf
hoehleatsu <hoehle@lillagubben>
2016-10-20 23:30:04 +0200

    changed names and recoded nyblom function to use different qnbinom approach - results should be the same

--- R/quantile_confints.R
+++ R/quantile_confints.R
@@ -1,68 +1,3 @@
-#' Quantile confidence interval based on interpolating the order
-#' statistic as suggested in Nyblom (1991)
-#'
-#' @param x vector of observations
-#' @param p quantile of interest, \eqn{0 \leq p \leq 1}{0 <= p <= 1}
-#' @param conf.level A conf.level * 100% confidence interval is
-#'   computed
-#' @param x_is_sorted Boolean (Default: FALSE) to safe sorting x, if
-#'   it is already sorted. This is merely for speed reasons in
-#'   situations where it is more efficient to only sort x once.
-#' @param interpolate Boolean (Default: TRUE) stating whether to
-#'   interpolate the order statistics. If no interpolation is selected
-#'   then this is just the standard exact procedure based on the order
-#'   statistics. Note: This procedure is conservative (i.e. coverage
-#'   is usualler larger than the nominal conf.level and hence the
-#'   interval is actually in general too large).
-#' @details The interpolation procedure suggested by Nyblom (1992),
-#'   which extends work by Hettmansperger and Sheather (1986) for the
-#'   median is applied to the order statistic.
-#' @examples
-#' set.seed(123)
-#' x <- rnorm(25)
-#' quantile_confint_nyblom(x=x, p=0.8, conf.level=0.95, interpolate=TRUE)
-#' @references Nyblom J, Note in interpolated order statistics,
-#'   Statistics and Probability Letters 14, p. 129-131.
-#' @importFrom stats dbinom pbinom qbinom
-#' @return A vector of length two containing the lower and upper limit
-#'   of the confidence interval
-#' @export
-
-quantile_confint_nyblom <- function(x, p, conf.level=0.95, x_is_sorted=FALSE, interpolate=TRUE) {
-  ##Define variables needed later
-  if (!x_is_sorted) { x <-sort(x) }
-  n <- length(x)
-  alpha <- 1-conf.level
-
-  ##Find d
-  (d <- n - qbinom(1-alpha/2, size=n, prob=(1-p)))
-  ##Find e
-  x_p <- qbinom(alpha/2, size=n, prob=(1-p))
-  subtract_one <- (x_p > 0) & (pbinom(x_p, size=n, prob=(1-p)) > alpha/2)
-  (e <- n - x_p + subtract_one)
-
-  d <- max(d,1)
-  e <- min(e,n)
-
-  ##If no interpolatation is to be done.
-  if (!interpolate) return(x[c(d,e)])
-
-  ##See Nyblom paper for the formula
-  lambda <- function(r, beta, p) {
-    pi_r <- dbinom(r-1, prob=p, size=n)
-    pi_rp1 <- dbinom(r, prob=p, size=n)
-    (1 + (r*(1-p)*(pi_rp1 - beta))/( (n-r)*p*(beta - pi_r)) )^(-1)
-  }
-  ci_limit <- function(r,beta) {
-    lambda <- lambda(r=r, beta=beta, p=p)
-    (1-lambda)*x[r] + lambda*x[pmin(r+1,n)]  #can't go beyond n
-  }
-  ##Return the Nyblom interval
-  return(c(ci_limit(d, beta=alpha/2),   ci_limit(e, beta=1-alpha/2)))
-}
-
-
-
 #' Confidence interval method for the median by the method of
 #' Hettmansperger & #' Sheather (1991)
 #'
@@ -89,7 +24,7 @@ quantile_confint_nyblom <- function(x, p, conf.level=0.95, x_is_sorted=FALSE, in
 #'   of the confidence interval
 #' @export
 #'
-confint_median_hs <- function(x, conf.level=0.95, x_is_sorted=FALSE, interpolate=TRUE) {
+median_confint_hs <- function(x, conf.level=0.95, x_is_sorted=FALSE, interpolate=TRUE) {
   if (!x_is_sorted) { x <- sort(x) }
   n <- length(x)
   d <- 1
@@ -141,6 +76,84 @@ confint_median_hs <- function(x, conf.level=0.95, x_is_sorted=FALSE, interpolate
 #' @export
 #'
 quantile_confint_boot <- function(x, p, conf.level=0.95, R=999) {
-  b <- boot::boot(x, statistic=function(data, idx) quantile(x[idx],prob=q,type=3), R=R)
+  b <- boot::boot(x, statistic=function(data, idx) quantile(x[idx],prob=p,type=3), R=R)
   boot::boot.ci(b, conf=conf.level, type="perc")$percent[4:5]
+}
+
+#' Quantile confidence interval based on interpolating the order
+#' statistic as suggested in Nyblom (1991)
+#'
+#' @param x vector of observations
+#' @param p quantile of interest, \eqn{0 \leq p \leq 1}{0 <= p <= 1}
+#' @param conf.level A conf.level * 100% confidence interval is
+#'   computed
+#' @param x_is_sorted Boolean (Default: FALSE) to safe sorting x, if
+#'   it is already sorted. This is merely for speed reasons in
+#'   situations where it is more efficient to only sort x once.
+#' @param interpolate Boolean (Default: TRUE) stating whether to
+#'   interpolate the order statistics. If no interpolation is selected
+#'   then this is just the standard exact procedure based on the order
+#'   statistics. Note: This procedure is conservative (i.e. coverage
+#'   is usualler larger than the nominal conf.level and hence the
+#'   interval is actually in general too large).
+#' @param fix_interval Boolean (Default: TRUE) For the case with no
+#'   interpolation, try to extend interval upwards if coverage is too
+#'   little.
+#' @details The interpolation procedure suggested by Nyblom (1992),
+#'   which extends work by Hettmansperger and Sheather (1986) for the
+#'   median is applied to the order statistic.
+#' @examples
+#' set.seed(123)
+#' x <- rnorm(25)
+#' quantile_confint_nyblom(x=x, p=0.8, conf.level=0.95, interpolate=TRUE)
+#' @references Nyblom J, Note in interpolated order statistics,
+#'   Statistics and Probability Letters 14, p. 129-131.
+#' @importFrom stats dbinom pbinom qbinom
+#' @return A vector of length two containing the lower and upper limit
+#'   of the confidence interval
+#' @export
+#'
+quantile_confint_nyblom <- function(x, p, conf.level=0.95, x_is_sorted=FALSE, interpolate=TRUE,fix_interval=TRUE) {
+  if (!x_is_sorted) { x <- sort(x) }
+  n <- length(x)
+  alpha <- 1-conf.level
+
+  ##Alternative implementation, which is probably faster
+  d <- qbinom( alpha/2, prob=p, size=n)
+  d <- d + (isTRUE(all.equal(pbinom(d, prob=p, size=n),alpha/2)))
+  e <- qbinom(1-alpha/2, size=n, prob=p) + 1
+
+  ##Safeguard
+  d <- max(d,1)
+  e <- min(e,n)
+
+  ##Stop here if no interpolation wanted.
+  ##If no interpolatation is to be done.
+  if (!interpolate) {
+    ##This part is borrowed from the EnvStats:::ci.qnpar procedure,
+    ##which is called as part of EnvStats::eqnpar (empirical quantile
+    ##non-parametric) If there is too little coverage for the
+    ##interval then increase length by one (if possible)
+    if (fix_interval & (p != 0) & (p != 1)) {
+      if ((pbinom(e, n, p) - pbinom(d - 1, n, p)) <= 1 - alpha) {
+        e <- min(n, e + 1)
+      }
+    }
+    return(x[c(d,e)])
+  }
+
+
+  ##See Nyblom (1992) paper for the formula
+  lambda <- function(r, beta, p) {
+    pi_r <- dbinom(r-1, prob=p, size=n)
+    pi_rp1 <- dbinom(r, prob=p, size=n)
+    (1 + (r*(1-p)*(pi_rp1 - beta))/( (n-r)*p*(beta - pi_r)) )^(-1)
+  }
+  ci_limit <- function(r,beta) {
+    lambda <- lambda(r=r, beta=beta, p=p)
+    (1-lambda)*x[r] + lambda*x[pmin(r+1,n)]  #can't go beyond n
+  }
+
+  ##Return the Nyblom interval
+  return(c(ci_limit(d, beta=alpha/2),   ci_limit(e, beta=1-alpha/2)))
 }
--- tests/testthat/test-quantile_confints.R
+++ tests/testthat/test-quantile_confints.R
@@ -17,7 +17,69 @@ x <- rnorm(10)
 for (p in seq(0,1,length=10)) {
   test_that("Manual summing up probs matching qbinom quantile",
             expect_equal(quantile_confint_so(x=x, p=p, conf.level=0.95),
-                         quantile_confint_nyblom(x=x, p=p, conf.level=0.95, interpolate=FALSE)))
+                         quantile_confint_nyblom(x=x, p=p, conf.level=0.95, interpolate=FALSE,fix_interval=FALSE)))
 }
 
+## set.seed(123)
+## for (n in c(10,25,100)) {
+##   for (p in seq(0,1,length=10)) {
+##     for (alpha in c(0.05, 0.1)) {
+##       test_that("Comparing the two Nyblom (1992) methods",
+##                 expect_equal(quantile_confint_nyblom(x=x, p=p, conf.level=1-alpha, interpolate=TRUE),
+##                              quantile_confint_nyblom2(x=x, p=p, conf.level=1-alpha, interpolate=TRUE)))
+##     }
+##   }
+## }
 
+x <- rnorm(11)
+quantile_confint_nyblom(x=x, p=0.5, conf.level=1-alpha, interpolate=TRUE)
+median_confint_hs(x=x, conf.level=1-alpha, interpolate=TRUE)
+
+#Sandbox test zone
+if (FALSE) {
+  EnvStats::eqnpar(x=x, p=p, ci=TRUE)
+  quantile_confint_hmm(x=x, p=p, conf.level=0.95)
+
+  ##Check prob P(x[2] <= xi_p <= x[5])
+  mean(replicate(1e5, {
+    xi_p <- qnorm(0.05)
+    x <- sort(rnorm(10))
+    (x[2] <= xi_p) & (xi_p <= x[5] )
+  }))
+  pbinom(5-1, size=10, prob=0.05) - pbinom(2-1, size=10, prob=0.05)
+
+  `%<=%` <- function(x,y) {
+    (x<y) | sapply(seq_len(length(x)), function(i) isTRUE(all.equal(x[i],y[i])))
+  }
+  `%>=%` <- function(x,y) {
+    (x>y) | sapply(seq_len(length(x)), function(i) isTRUE(all.equal(x[i],y[i])))
+  }
+
+  n <- length(x)
+  r <- 1:n
+  p <- 0.5
+  alpha <- 1
+
+  c(5,2) %<=% c(3,1)
+  c(5,2) %>=% c(7,1)
+
+  idx <- which((1 - pbinom(r-1, prob=p, size=n)) %>=% (1 - alpha/2))
+  idx
+  idx <- which( pbinom(r-1, prob=p, size=n) %<=% (alpha/2))
+  idx
+  max(idx)
+  d <- qbinom( alpha/2, prob=p, size=n)
+  d <- d + (isTRUE(all.equal(pbinom(d, prob=p, size=n),alpha/2)))
+  d
+
+  idx <- which( pbinom(r-1, prob=p, size=n) >= 1-alpha/2)
+  e <- min(idx)
+
+  1 - (1-pbinom(e-1,size=n, prob=p))
+
+  c(e,  qbinom(1-alpha/2, size=n, prob=p) + 1)
+
+  pbinom(e-1, size=n, prob=p) - pbinom(d-1, size=n, prob=p)
+
+
+}
