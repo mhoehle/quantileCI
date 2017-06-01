1a16dcac4cb44e579e0174ec56f9df1655f70523
hoehleatsu <hoehle@math.su.se>
2017-06-01 22:42:58 +0200

    small changes in the test script

--- tests/testthat/test-quantile_confints.R
+++ tests/testthat/test-quantile_confints.R
@@ -91,7 +91,7 @@ if (FALSE) {
   ## Compare with Table 1 of Nyblom (1992)
   ######################################################################
   set.seed(123); n<- 11; x <- sort(rnorm(n)) ; p <- 0.25 ; conf.level=0.9
-  (ci <- quantile_confint_nyblom(x, p=p, conf.level=conf.level, interpolate=FALSE))
+  (ci <- quantileCI::quantile_confint_nyblom(x, p=p, conf.level=conf.level, interpolate=FALSE))
   (idx <- pmatch(ci, x))
 
   #Tail probabilities
@@ -106,7 +106,7 @@ if (FALSE) {
   q_truth <- qnorm(p)
   (1-lambda) * x[1] + lambda*x[2]
 
-  quantile_confint_nyblom(x, p=p, conf.level=0.9)
+  quantileCI::quantile_confint_nyblom(x, p=p, conf.level=0.9)
 
   ##Selection of methods to use
   quantile_confints <- function(x, p, conf.level, x_is_sorted=FALSE, ...) {
@@ -114,16 +114,20 @@ if (FALSE) {
 
     ##Compute the various confidence intervals as above
     res <- data.frame(
-                      EnvStats_asymp=as.numeric(EnvStats::eqnpar(x=x, p=p, ci=TRUE, ci.method="normal.approx",approx.conf.level=conf.level)$interval$limits),
-                      nyblom_interp=quantileCI::quantile_confint_nyblom(x=x, p=p, conf.level=conf.level,x_is_sorted=TRUE,interpolate=TRUE)
-                      )
+      EnvStats_asymp=as.numeric(EnvStats::eqnpar(x=x, p=p, ci=TRUE, ci.method="normal.approx",approx.conf.level=conf.level)$interval$limits),
+      nyblom_exact=quantileCI::quantile_confint_nyblom(x=x, p=p, conf.level=conf.level,x_is_sorted=TRUE,interpolate=FALSE),
+      nyblom_interp=quantileCI::quantile_confint_nyblom(x=x, p=p, conf.level=conf.level,x_is_sorted=TRUE,interpolate=TRUE)
+    )
+
     if (p == 0.5) {
       res$hs_interp = quantileCI::median_confint_hs(x=x,  conf.level=conf.level,x_is_sorted=TRUE,interpolate=TRUE)
     }
     return(res)
   }
 
-  simulate.coverage_qci <- function(n=n,p=p,conf.level=0.9, nSim=1e4, ...) {
+  require(dplyr)
+
+  simulate.coverage_qci <- function(n=n,p=p,conf.level=0.9, nSim=25e3, ...) {
     ##Windows users: change to below lapply function or use snow.
     lapplyFun <- function(x, mc.cores=NA, ...) lapply(x, ...) #parallel::mclapply
     ##lapplyFun <- parallel::mclapply
@@ -152,13 +156,14 @@ if (FALSE) {
   ci1 <- quantileCI::quantile_confint_nyblom(x, p=p, interpolate=FALSE, conf.level=1-alpha)
   ci2 <- quantileCI::quantile_confint_nyblom(x, p=p, interpolate=TRUE, conf.level=1-alpha)
 
-  idx <- pmatch(ci1, x)
+  ##Compute coverage for smoothed interval by Monte Carlo sampling
+  ##(this might be slow) Try to do this using numerical integration
+  ##instead.
 
-  ##Compute coverage for smoothed interval by Monte Carlo sampling (this might be slow)
-  ##Try to do this using numerical integration instead.
-  d <- 17 ; e <- 24
-  lambda1 <- 0.09313647
-  lambda2 <- 0.4259059
+  idx <- pmatch(ci1, x)
+  d <- idx[1]
+  e <- idx[2]
+
   ##See Nyblom (1992) paper for the formula
   lambda <- function(r, beta, p) {
     pi_r   <- pbinom(r-1, prob=p, size=n)
@@ -166,18 +171,17 @@ if (FALSE) {
 
     (1 + (r*(1-p)*(pi_rp1 - beta))/( (n-r)*p*(beta - pi_r)) )^(-1)
   }
-  d <- idx[1]
-  e <- idx[2]
+
+
   lambda1 <- lambda(r=d, beta=alpha/2, p=p)
   lambda2 <- lambda(r=e-1, beta=1-alpha/2, p=p)
-
+  c(lambda1,lambda2)  ## lambda1 <- 0.09313647, ## lambda2 <- 0.4259059
 
   tails  <- replicate(1e6, {
     y <- sort(rnorm(n))
     ##These numbers are computed by the Nyblom procedure. Computed manually
     ##from the functions defined inside the quantile_confint_nyblom function.
     c((1-lambda1)*y[d] + lambda1*y[d+1] > x_p, (1-lambda2)*y[e-1] + lambda2*y[e] <= x_p)
-
   })
   dim(tails)
   rowMeans(tails)
@@ -191,24 +195,57 @@ if (FALSE) {
   ##Tail probs of the weighted sum, i.e. compute
   ##  P( (1-\lambda) x_{(r)} + lambda * x_{(r+1)} > xi_p)
   p_wsum <- function(xi_p, r,lambda, F, f) {
+    ##See https://en.wikipedia.org/wiki/Order_statistic for density
+    f_joint <- function(xr, xrp1, r) {
+      (xrp1 > xr) * exp(lfactorial(n) - lfactorial(r-1) - lfactorial(n - r -1)) * F(xr)^(r-1) * (1-F(xrp1))^(n-r-1)*f(xr)*f(xrp1)
+    }
+
+    grid <- expand.grid(xr=xr <- seq(-50,20,length=1000),xrp1=xr)
+    y <- f_joint(grid$xr, grid$xrp1, r=d)
+    image(xr,xr,matrix(log(y), ncol=length(xr),nrow=length(xr)))
+    xr <- seq(-40,20,length=100)
+    z <- x_p
+    xrp1 <- (z - (1-lambda)*xr)/lambda
+    points(xr, xrp1,type="l",lwd=2)
+
+    sum(f_joint(grid$xr, grid$xrp1, r=d)) * (xr[2] - xr[1])^2
+
+    sum(f_joint(xr, grid$xrp1, r=d)) * (xr[2] - xr[1])^2
+
     ##Function giving the PMF of z = (1-\lambda) x_{(r)} + lambda * x_{(r+1)}
     f_z <- function(z) {
+      ##Convolution of two dependent random variables
       f_to_int <- function(xr,z) {
         xrp1 <- (z - (1-lambda)*xr)/lambda
-        ##consistency check: (1-lambda)*xr + (lambda)*xrp1
-        ##See https://en.wikipedia.org/wiki/Order_statistic for density
-        (xrp1 > xr) * exp(lfactorial(n) - lfactorial(r-1) - lfactorial(n - r -1)) * F(xr)^(r-1) * (1-F(xrp1))^(n-r-1)*f(xr)*f(xrp1)
-        ##(xrp1 > xr) * exp(lfactorial(n) - lfactorial(r-1) - lfactorial(n - r -1) + (r-1)*log(F(xr)) + (n-r-1)*log((1-F(xrp1))) + log(f(xr)) + log(f(xrp1)))
+        if (!(isTRUE(all.equal((1-lambda)*xr + (lambda)*xrp1, rep(z,length(xr)))))) { #cons. check
+          browser()
+        }
+        if (!all((xrp1 > xr))) {  browser()}
+        f_joint(xr, xrp1, r)
       }
-      one <- function(z) {
-        integrate( f_to_int, lower=-Inf, upper=z, z=z)$value
+      ##Density PDF of z = (1-lambda)*xr + (lambda)*xrp1
+      fz_one <- function(z) {
+        integrate( f_to_int, lower=-Inf, upper=z, z=z,abs.tol=1e-12)$value
       }
-      sapply(z, one)
+
+      ## print(z)
+      ## xr_grid <- seq(-10,z[2],length=1000)
+      ## plot(xr_grid, sapply(xr_grid, fZ),type="l")
+
+      sapply(z, fz_one)
     }
+
+
+    z_grid <- seq(-15,15,length=1000)
+    plot(z_grid, sapply(z_grid, f_z),type="l")
+    integrate(f_z, lower=-5,upper=Inf,abs.tol=1e-12)$value
+
     #Calculate P(z >= x_p) by numerical integration
-    integrate(f_z, lower=x_p,upper=10)$value
+    integrate(f_z, lower=x_p,upper=Inf,abs.tol=1e-12)$value
   }
   #Get the two tail probabilities for the procedure
   p_wsum( x_p, r=d, lambda=lambda1,   F=pnorm, f=dnorm)
   p_wsum( x_p, r=e-1, lambda=lambda2, F=pnorm, f=dnorm)
+
+
 }
